435ba8d7a2f5358167b6b65042bea18c7136dac5
Martin Kearn <martin.kearn@microsoft.com>
2022-07-07 16:10:57 +0100

    Unit test tests code repo container

---
+++ src/WeatherForecastTests.cs
@@ -0,0 +1,12 @@
+﻿namespace BasicTest;
+
+public class WeatherForecastTests
+{
+    [Fact]
+    public async Task WeatherForecastReturnsOK()
+    {
+        var client = new HttpClient();
+        var response = await client.GetAsync("http://localhost:9012/weatherforecast");
+        Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+    }
+}
